e5406948278b0e26e51cbf5be27745cb2e08d56e
GabDer <gabriel.deroeux@hotmail.fr>
2022-03-03 10:01:02 +0100

    stable

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
 class SecurityController extends AbstractController
@@ -11,9 +12,9 @@ class SecurityController extends AbstractController
     /**
      * @Route("/", name="app_login")
      */
-    public function login(): Response
+    public function login(Request $request)
     {
-        return $this->render('templates/login.html.twig');
+        return $this->render('/login.html.twig');
     }
 
     /**
