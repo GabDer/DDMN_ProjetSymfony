7e86de8620ccbde5aaa129f99e5f318ad91901af
GabDer <gabriel.deroeux@hotmail.fr>
2022-03-15 09:06:00 +0100

    PageLogin

--- src/Controller/SecurityController.php
+++ src/Controller/SecurityController.php
@@ -6,15 +6,23 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Form\LoginFormType;
+use App\Entity\User;
 
 class SecurityController extends AbstractController
 {
     /**
      * @Route("/", name="app_login")
      */
-    public function login(Request $request)
-    {
-        return $this->render('/login.html.twig');
+    public function Login(Request $request){
+        $form = $this->createForm(LoginFormType :: class);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()){
+            $annonceInfos = $form->getData();
+            // On vérifie que les infos correspondent
+            var_dump($annonceInfos);
+        }
+        return $this->render('login.html.twig', ['loginform' => $form->createView()]);
     }
 
     /**
