59cef411ca9ef02c16eeb026600a2c3c98e3c260
YorimNNN <yorim.niabe@campus-la-chataigneraie.org>
2022-05-10 16:42:37 +0200

    certaines modifs

--- src/Controller/ListeEntrepriseController.php
+++ src/Controller/ListeEntrepriseController.php
@@ -34,7 +34,6 @@ class ListeEntrepriseController extends AbstractController
         foreach ($listeEntreprises as $entreprise){ //Pour chaque entreprise, on y associe un tableau de ses personnes dans le tableau 'listePersonnes'
             $listePersonnes = array_merge($listePersonnes,$entityManager->getRepository(ENTREPRISE::class)->AffichagePersonnesEntreprise($entreprise['ent_raison_sociale'])); //array_merge permet d'ajouter des éléments à un tableau déja existant
         }
-        dd($listeSpecialite);
         //dd($listeEntreprises);
         if (isset($_GET['ParamRecue']))
             return $this->render('/ListeEntreprise.html.twig', ['listeEntreprises' => $listeEntreprises, 'listePersonnes' => $listePersonnes, 'listeSpecialite' => $listeSpecialite, 'ParamRecue' => $_GET['ParamRecue']]);
